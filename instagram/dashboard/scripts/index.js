5255ff2061969698b3d5c9b8b8d76a7e0af7fa92
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-05-08 12:53:02 +0900

    make layout, add modal script

---
+++ instagram/dashboard/scripts/index.js
@@ -0,0 +1,27 @@
+const dots = document.querySelector(".user__dots");
+const modal = document.querySelector(".modal");
+const modalList = document.querySelectorAll(".modal__tags");
+
+function handleClick() {
+  if (modal.classList.contains("hide")) {
+    modal.classList.remove("hide");
+  } else {
+    modal.classList.add("hide");
+  }
+}
+
+function handleOutsideClick(event) {
+  if (event.target !== dots) {
+    let flag = 0;
+    for (let i = 0; i < 6; i++) {
+      if (event.target === modalList[i]) {
+        flag = 1;
+        break;
+      }
+    }
+    if (!flag) modal.classList.add("hide");
+  }
+}
+
+dots.addEventListener("click", handleClick);
+window.addEventListener("click", handleOutsideClick);
